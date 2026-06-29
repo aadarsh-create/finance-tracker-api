283a2f051eadc3c07262990b20eedc843f23c95d
aadarsh-create <p.aadarsh2006@gmail.com>
2026-06-29 18:52:41 +0530

    add transaction router

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from transaction import router
+from transaction import transaction_router
 
 myapp = FastAPI(title="Finance Tracker API",version='1.0')
-myapp.include_router(router)
+myapp.include_router(transaction_router)
