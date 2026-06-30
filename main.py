90417b5300ae3c53dea47849f2288880f0fb267c
aadarsh-create <p.aadarsh2006@gmail.com>
2026-06-30 23:34:41 +0530

    add more routers

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+
 from transaction import transaction_router
+from summary import summary_router
+from insights import insight_router
+
+
 
 myapp = FastAPI(title="Finance Tracker API",version='1.0')
+
 myapp.include_router(transaction_router)
+myapp.include_router(summary_router)
+myapp.include_router(insight_router)
