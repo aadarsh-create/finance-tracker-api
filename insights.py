ab846ed9e9997bdb9ee666b59a6241938c5555e8
aadarsh-create <p.aadarsh2006@gmail.com>
2026-07-01 22:15:27 +0530

    fix bug: error on empty expense transaction

--- insights.py
+++ insights.py
@@ -1,13 +1,20 @@
 from fastapi import APIRouter
 from database import load_db
-from summary import get_summary_by_category,get_summary,get_monthly_summary
+from summary import get_summary_by_category,get_summary
 from datetime import date
+from fastapi import HTTPException
 
 insight_router = APIRouter()
 
 @insight_router.get('/insights')
 def get_insights():
     transactions = load_db()['transactions'].values()
+    expense_transactions = [t for t in transactions if t['type']=='expense']
+    if len(expense_transactions)==0:
+        raise HTTPException(
+            status_code=400,
+            detail='Must Have atleast One Expense On transactions'
+        )
 
     data = get_summary_by_category(type='expense')
     top_category = [ key for key in data.keys() if data[key] == max(data.values()) ][0]
