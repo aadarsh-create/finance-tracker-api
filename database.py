a43b4727b58cf366c818be507bb83ec793a75f1b
aadarsh-create <p.aadarsh2006@gmail.com>
2026-07-01 22:29:09 +0530

    fix bug: error if .env is missing

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-db_path = os.getenv("DB_PATH")
+db_path = os.getenv("DB_PATH", "db.json")
 
 def load_db():
     with open(db_path,'r') as file_data:
