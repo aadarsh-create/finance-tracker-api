a6cd584bc7f58b3baa5c79b697db99b3ffa52e13
aadarsh-create <p.aadarsh2006@gmail.com>
2026-07-23 00:21:26 +0530

    add predict router

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from transaction import transaction_router
 from summary import summary_router
 from insights import insight_router
+from predict import predict_router
 
 
 
@@ -11,3 +12,4 @@ myapp = FastAPI(title="Finance Tracker API",version='1.0')
 myapp.include_router(transaction_router)
 myapp.include_router(summary_router)
 myapp.include_router(insight_router)
+myapp.include_router(predict_router)
